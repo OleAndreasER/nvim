aab68958cac83591594c3a0a9af6b4bcb79f184d
oarorvik <oarorvik@stud.ntnu.no>
2024-10-28 20:02:08 +0100

    git branch stuff

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -80,6 +80,7 @@ require("lazy").setup({
 		config = function()
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.git_files)
+			vim.keymap.set('n', '<leader>fc', builtin.git_branches)
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 			vim.keymap.set('n', '<leader>fs', builtin.grep_string)
 		end
