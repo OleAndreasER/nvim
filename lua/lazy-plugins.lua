d2126efa3d4f204c3a8f761243fe13da82178d2d
oarorvik <oarorvik@stud.ntnu.no>
2024-10-28 18:23:34 +0100

    harpoon

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -60,6 +60,18 @@ require("lazy").setup({
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>")
 		end,
 	},
+	-- Harpoon
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function ()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+			vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+			vim.keymap.set("n", "<C-k>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		end
+	},
 	-- Searching 
     {
 		'nvim-telescope/telescope.nvim',
