0770004e40222eefc8a41de48d7d64b738da6b12
Ole Andreas Egeland Rørvik <ole.rorvik@hent.no>
2024-11-04 08:58:39 +0100

    nice plugins

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+	'Wansmer/treesj',
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	config = function()
+		local treesj = require('treesj')
+		treesj.setup({
+			use_default_keymaps = false,
+			max_join_length = 100000000000000000,
+		})
+		vim.keymap.set('n', '<leader>m', treesj.toggle)
+	end,
+}
