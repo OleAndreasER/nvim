869510869155bc3f62b5dd3da4dcb2af7490f8f5
oarorvik <oarorvik@stud.ntnu.no>
2025-01-03 13:33:45 +0100

    simpler quickfixing

--- lua/options.lua
+++ lua/options.lua
@@ -59,22 +59,5 @@ vim.keymap.set('n', '<C-j>', function() vim.lsp.buf.hover() end)
 vim.keymap.set('n', 'ø', '<C-w>l')
 vim.keymap.set('n', 'å', '<C-w>h')
 -- Quickfix
-vim.keymap.set('n', '<leader>cn', '<CMD>cn<CR>')
-vim.keymap.set('n', '<leader>cp', '<CMD>cp<CR>')
-vim.keymap.set('n', '<leader>cc', function()
-	local qf_exists = false
-	for _, win in pairs(vim.fn.getwininfo()) do
-		
-		if win["quickfix"] == 1 then
-			qf_exists = true
-		end
-	end
-	if qf_exists == true then
-		vim.cmd("cclose")
-		return
-	end
-	if not vim.tbl_isempty(vim.fn.getqflist()) then
-		vim.cmd("copen")
-	end
-end)
+vim.keymap.set('n', '<leader>c', '<CMD>copen<CR>')
 
