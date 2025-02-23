5876b9117165db3f2c0f198859b3d350f04cba43
OleAndreasER <oleaegeland@gmail.com>
2025-02-23 07:33:38 +0100

    snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = false },
+		dashboard = { enabled = false },
+		explorer = { enabled = false },
+		input = { enabled = false },
+		picker = { enabled = false },
+		notifier = { enabled = true },
+		quickfile = { enabled = false },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+
+		indent = {
+			animate = { enabled = false },
+			scope = { enabled = false },
+		}
+	},
+	keys = {
+	   { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+	},
+}
