ad30021005ce5cd3335d2236fdf8503355040408
OleAndreasER <oleaegeland@gmail.com>
2025-03-29 22:30:53 +0100

    bigfiles

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,7 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
-		bigfile = { enabled = false },
+		bigfile = { enabled = true },
 		dashboard = { enabled = false },
 		explorer = { enabled = false },
 		input = { enabled = false },
