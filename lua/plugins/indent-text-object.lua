1c8a9f2c65bbaa762d7b38ed1000793dfb45a0c0
oarorvik <oarorvik@stud.ntnu.no>
2025-04-27 09:26:07 +0200

    simplify indent text object

--- lua/plugins/indent-text-object.lua
+++ lua/plugins/indent-text-object.lua
@@ -3,27 +3,15 @@ return {
 	keys = {
 		{
 			"ai",
-			function() require('treesitter_indent_object.textobj').select_indent_outer() end,
-			mode = {"x", "o"},
-			desc = "Select context-aware indent (outer)",
-		},
-		{
-			"aI",
 			function() require('treesitter_indent_object.textobj').select_indent_outer(true) end,
 			mode = {"x", "o"},
-			desc = "Select context-aware indent (outer, line-wise)",
+			desc = "",
 		},
 		{
 			"ii",
-			function() require('treesitter_indent_object.textobj').select_indent_inner() end,
+			function() require('treesitter_indent_object.textobj').select_indent_inner(true) end,
 			mode = {"x", "o"},
-			desc = "Select context-aware indent (inner, partial range)",
-		},
-		{
-			"iI",
-			function() require('treesitter_indent_object.textobj').select_indent_inner(true, 'V') end,
-			mode = {"x", "o"},
-			desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+			desc = "",
 		},
 	},
 }
