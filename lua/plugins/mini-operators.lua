e3ff6f1ed2eb18ce9fceb97c89a187e54d3a46ba
OleAndreasER <oleaegeland@gmail.com>
2025-05-10 10:51:58 +0200

    remap op operators

--- lua/plugins/mini-operators.lua
+++ lua/plugins/mini-operators.lua
@@ -3,6 +3,19 @@ return {
 	version = false,
 	config = function ()
 		require('mini.operators').setup({
+
+			-- Multiply (duplicate) text
+			multiply = {
+				prefix = 'M',
+				func = nil,
+			},
+
+			-- Replace text with register
+			replace = {
+				prefix = 'R',
+				reindent_linewise = true,
+			},
+
 		})
 	end
 }
