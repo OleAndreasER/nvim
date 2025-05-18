9ee6fcdc468ed2919376537452025d81d130569f
OleAndreasER <oleaegeland@gmail.com>
2025-05-18 14:57:32 +0200

    switch to snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,17 +1,67 @@
+local exclude = {
+	"node_modules/",
+	"dist/",
+	".next/",
+	".vite/",
+	".git/",
+	".gitlab/",
+	"build/",
+	"target/",
+	"package-lock.json",
+	"pnpm-lock.yaml",
+	"yarn.lock",
+}
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
 	---@type snacks.Config
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		bigfile = { enabled = true },
 		dashboard = { enabled = false },
 		explorer = { enabled = false },
 		input = { enabled = false },
-		picker = { enabled = false },
+		picker = {
+			layout = {
+				preset = 'dropdown_main_preview',
+				cycle = false,
+			},
+			layouts = {
+				dropdown_main_preview = {
+					preview = "main",
+					layout = {
+						backdrop = false,
+						row = 1,
+						width = 0.2,
+						min_width = 40,
+						height = 0.4,
+						border = "none",
+						box = "vertical",
+						{ win = "input", height = 1, border = "rounded", title = "{title} {live} {flags}", title_pos = "center" },
+						{ win = "list", border = "hpad" },
+						{ win = "preview", title = "{preview}", border = "rounded" },
+					},
+				},
+			},
+			formatters = {
+				file = {
+					filename_first = false,
+					filename_only = true,
+				},
+			},
+			matcher = {
+				fuzzy = true,
+				smartcase = false,
+				ignorecase = false,
+				sort_empty = false,
+				filename_bonus = true,
+				file_pos = true,
+				cwd_bonus = false,
+				frecency = false,
+				history_bonus = false,
+			},
+		},
 		notifier = { enabled = true },
 		quickfile = { enabled = false },
 		scope = { enabled = false },
@@ -24,6 +74,55 @@ return {
 			scope = { enabled = false },
 		}
 	},
-	config = function()
-	end,
+	keys = {
+		{ "<leader>s", function() Snacks.picker.grep({
+			layout = 'ivy_split',
+			finder = "grep",
+			regex = false,
+			format = "file",
+			show_empty = true,
+			live = true,
+			supports_live = true,
+			hidden = true,
+			ignored = true,
+			exclude = exclude,
+			smartcase = false,
+			ignorecase = false,
+		}) end },
+
+		{ "<leader>f", function() Snacks.picker.files({
+			finder = "files",
+			format = "file",
+			show_empty = true,
+			hidden = true,
+			ignored = true,
+			follow = false,
+			supports_live = true,
+			exclude = exclude,
+		}) end },
+
+		{ "<leader>b", function() Snacks.picker.git_branches({
+			all = false,
+			finder = "git_branches",
+			format = "git_branch",
+			preview = "git_log",
+			confirm = "git_switch",
+			win = {input = {keys = {}}},
+			on_show = function(picker) for i, item in ipairs(picker:items()) do if item.current then picker.list:view(i) Snacks.picker.actions.list_scroll_center(picker) break end end end,
+		}) end },
+
+		{ "<leader>G", function() Snacks.picker.git_status({
+			finder = "git_status",
+			format = "git_status",
+			preview = "git_status",
+			win = { input = { keys = { ["-"] = { "git_stage", mode = { "n", "i" } } } } },
+		}) end },
+
+		{ "<leader>/", function() Snacks.picker.lines() end },
+		{ "<leader>h", function() Snacks.picker.help() end },
+		{ "<leader><space>", function() Snacks.picker.resume() end },
+		{ "<leader>d", function() Snacks.picker.lsp_definitions() end },
+		{ "<leader>r", function() Snacks.picker.lsp_references() end },
+		{ "<leader>l", function() Snacks.picker.lazy() end },
+	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -83,128 +83,128 @@ return {
 			return '<cmd>Telescope ' .. x .. '<cr>'
 		end
 
-		vim.keymap.set({ 'n', 'v' }, '<leader>d', search("lsp_definitions"))
-		vim.keymap.set({ 'n', 'v' }, '<leader>f', search("find_files"))
-		vim.keymap.set({ 'n', 'v' }, '<leader>r',  browse('lsp_references'))
-		vim.keymap.set({ 'n', 'v' }, '<leader><leader>', browse('resume') )
-		vim.keymap.set({ 'n', 'v' }, '<leader>/', browse('project'))
-		vim.keymap.set({ 'n', 'v' }, '<leader>ag', ':Telescope advanced_git_search')
-		vim.keymap.set({ 'n', 'v' }, '<leader>b', search('git_branches'))
-		vim.keymap.set({ 'n', 'v' }, "<leader>'", browse('recall'))
-		vim.keymap.set("x", "<C-s>", '"zy<Cmd>lua require("telescope.builtin").grep_string({search=vim.fn.getreg("z")})<cr><c-q>')
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>d', search("lsp_definitions"))
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>f', search("find_files"))
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>r',  browse('lsp_references'))
+		-- vim.keymap.set({ 'n', 'v' }, '<leader><leader>', browse('resume') )
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>/', browse('project'))
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>ag', ':Telescope advanced_git_search')
+		-- vim.keymap.set({ 'n', 'v' }, '<leader>b', search('git_branches'))
+		-- vim.keymap.set({ 'n', 'v' }, "<leader>'", browse('recall'))
+		-- vim.keymap.set("x", "<C-s>", '"zy<Cmd>lua require("telescope.builtin").grep_string({search=vim.fn.getreg("z")})<cr><c-q>')
 		-- Quickfix (I think there was a reason to put this here I don't remember)
-		vim.keymap.set({ 'n', 'v' }, '<C-x>', '<cmd>cnext<cr><cmd>lua update_quickfix_display()<cr>')
-		vim.keymap.set({ 'n', 'v' }, '<C-z>', '<cmd>cprevious<cr><cmd>lua update_quickfix_display()<cr>')
+		-- vim.keymap.set({ 'n', 'v' }, '<C-x>', '<cmd>cnext<cr><cmd>lua update_quickfix_display()<cr>')
+		-- vim.keymap.set({ 'n', 'v' }, '<C-z>', '<cmd>cprevious<cr><cmd>lua update_quickfix_display()<cr>')
 
 		-- Custom grep
-		vim.keymap.set("n", "<leader>s", function(opts)
-			opts = opts or {}
-			opts.cwd = opts.cwd or vim.uv.cwd()
-
-			require("telescope.pickers")
-				.new(opts, {
-					prompt_title = "Search",
-					debounce = 100,
-					previewer = require("telescope.config").values.grep_previewer(opts),
-					sorter = require("telescope.sorters").empty(),
-					finder = require('telescope.finders').new_async_job({
-						command_generator = function(prompt)
-							if not prompt or prompt == "" then
-								return nil
-							end
-
-							local arguments, text = prompt:match("(%S*)%s?(.*)")
-							local args = { "rg" }
-
-							if text ~= '' then
-								table.insert(args, "-e")
-								table.insert(args, text)
-							else
-								return nil
-							end
-
-							if arguments ~= '' then
-								local location, second_part = arguments:match("([hra]?)(.*)")
-								local filetype = second_part:match("%..+")
-								local glob
-
-								if filetype ~= nil then
-									local parts = {}
-									for part in filetype:gmatch("([^%.]+)") do
-										table.insert(parts, part)
-									end
-									filetype = '.' .. '{' .. table.concat(parts, ',') .. '}'
-								end
-
-								if location == 'r' and filetype == nil then
-									glob = 'services/report-maker/**'
-								elseif location == 'h' and filetype == nil then
-									glob = 'apps/hent-core/**'
-								elseif location == 'a' and filetype == nil then
-									glob = 'services/restful-api/**'
-								elseif location == 'r' and filetype ~= nil then
-									glob = 'services/report-maker/**/*'..filetype
-								elseif location == 'h' and filetype ~= nil then
-									glob = 'apps/hent-core/**/*'..filetype
-								elseif location == 'a' and filetype ~= nil then
-									glob = 'services/restful-api/**/*'..filetype
-								elseif location == '' and filetype ~= nil then
-									glob = '*'..filetype
-								else
-									goto no_args
-								end
-
-								table.insert(args, "-g")
-								table.insert(args, glob)
-							end
-							::no_args::
-
-							return vim.tbl_flatten {
-								args,
-								{
-									"--color=never",
-									"--no-heading",
-									"--with-filename",
-									"--line-number",
-									"--column",
-									"--smart-case",
-									'--ignore-file',
-									'.gitignore'
-								},
-							}
-						end,
-						entry_maker = function (entry)
-							local vimgrep_entry = require("telescope.make_entry").gen_from_vimgrep({
-								disable_coordinates = true,
-							})(entry)
-
-							local old_display = vimgrep_entry.display
-							vimgrep_entry.display = function (e)
-								local raw, style = old_display(e)
-								local file, line = raw:match("^([^:]+):[^%S]*(.+)$")
-
-								local len_filename = 30
-								local const_len_filename
-								if #file > len_filename then
-									const_len_filename = file:sub(1, len_filename)
-								elseif #file < len_filename then
-									const_len_filename = file .. string.rep(" ", len_filename - #file)
-								else
-									const_len_filename = file
-								end
-
-								local len_line = 150
-								line = line:sub(1, len_line)
-
-								return const_len_filename .. '║' .. line, style
-							end
-
-							return vimgrep_entry;
-						end,
-					}),
-				})
-				:find()
-		end)
+		-- vim.keymap.set("n", "<leader>s", function(opts)
+		-- 	opts = opts or {}
+		-- 	opts.cwd = opts.cwd or vim.uv.cwd()
+		--
+		-- 	require("telescope.pickers")
+		-- 		.new(opts, {
+		-- 			prompt_title = "Search",
+		-- 			debounce = 100,
+		-- 			previewer = require("telescope.config").values.grep_previewer(opts),
+		-- 			sorter = require("telescope.sorters").empty(),
+		-- 			finder = require('telescope.finders').new_async_job({
+		-- 				command_generator = function(prompt)
+		-- 					if not prompt or prompt == "" then
+		-- 						return nil
+		-- 					end
+		--
+		-- 					local arguments, text = prompt:match("(%S*)%s?(.*)")
+		-- 					local args = { "rg" }
+		--
+		-- 					if text ~= '' then
+		-- 						table.insert(args, "-e")
+		-- 						table.insert(args, text)
+		-- 					else
+		-- 						return nil
+		-- 					end
+		--
+		-- 					if arguments ~= '' then
+		-- 						local location, second_part = arguments:match("([hra]?)(.*)")
+		-- 						local filetype = second_part:match("%..+")
+		-- 						local glob
+		--
+		-- 						if filetype ~= nil then
+		-- 							local parts = {}
+		-- 							for part in filetype:gmatch("([^%.]+)") do
+		-- 								table.insert(parts, part)
+		-- 							end
+		-- 							filetype = '.' .. '{' .. table.concat(parts, ',') .. '}'
+		-- 						end
+		--
+		-- 						if location == 'r' and filetype == nil then
+		-- 							glob = 'services/report-maker/**'
+		-- 						elseif location == 'h' and filetype == nil then
+		-- 							glob = 'apps/hent-core/**'
+		-- 						elseif location == 'a' and filetype == nil then
+		-- 							glob = 'services/restful-api/**'
+		-- 						elseif location == 'r' and filetype ~= nil then
+		-- 							glob = 'services/report-maker/**/*'..filetype
+		-- 						elseif location == 'h' and filetype ~= nil then
+		-- 							glob = 'apps/hent-core/**/*'..filetype
+		-- 						elseif location == 'a' and filetype ~= nil then
+		-- 							glob = 'services/restful-api/**/*'..filetype
+		-- 						elseif location == '' and filetype ~= nil then
+		-- 							glob = '*'..filetype
+		-- 						else
+		-- 							goto no_args
+		-- 						end
+		--
+		-- 						table.insert(args, "-g")
+		-- 						table.insert(args, glob)
+		-- 					end
+		-- 					::no_args::
+		--
+		-- 					return vim.tbl_flatten {
+		-- 						args,
+		-- 						{
+		-- 							"--color=never",
+		-- 							"--no-heading",
+		-- 							"--with-filename",
+		-- 							"--line-number",
+		-- 							"--column",
+		-- 							"--smart-case",
+		-- 							'--ignore-file',
+		-- 							'.gitignore'
+		-- 						},
+		-- 					}
+		-- 				end,
+		-- 				entry_maker = function (entry)
+		-- 					local vimgrep_entry = require("telescope.make_entry").gen_from_vimgrep({
+		-- 						disable_coordinates = true,
+		-- 					})(entry)
+		--
+		-- 					local old_display = vimgrep_entry.display
+		-- 					vimgrep_entry.display = function (e)
+		-- 						local raw, style = old_display(e)
+		-- 						local file, line = raw:match("^([^:]+):[^%S]*(.+)$")
+		--
+		-- 						local len_filename = 30
+		-- 						local const_len_filename
+		-- 						if #file > len_filename then
+		-- 							const_len_filename = file:sub(1, len_filename)
+		-- 						elseif #file < len_filename then
+		-- 							const_len_filename = file .. string.rep(" ", len_filename - #file)
+		-- 						else
+		-- 							const_len_filename = file
+		-- 						end
+		--
+		-- 						local len_line = 150
+		-- 						line = line:sub(1, len_line)
+		--
+		-- 						return const_len_filename .. '║' .. line, style
+		-- 					end
+		--
+		-- 					return vimgrep_entry;
+		-- 				end,
+		-- 			}),
+		-- 		})
+		-- 		:find()
+		-- end)
 
 		require("telescope").load_extension("zf-native")
 		require("telescope").load_extension("advanced_git_search")
