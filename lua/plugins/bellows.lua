39aa40d5e60fb3516a07f5fec88245fee6902f90
OleAndreasERHENT <ole.rorvik@hent.no>
2026-04-30 23:10:42 +0200

    kulala bellows

---
+++ lua/plugins/bellows.lua
@@ -0,0 +1,56 @@
+return {
+	"skyppex/bellows.nvim",
+	config = function()
+		require("bellows").setup({
+			line_count = true,
+			array_count_threshold = 3,
+			array_count_threshold_folded = 0,
+		})
+
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			callback = function(args)
+				local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+				if filetype ~= "json" then return end
+
+				local function toggle(condition, false_func, true_func)
+					return function()
+						if condition() then
+							true_func()
+						else
+							false_func()
+						end
+					end
+				end
+
+				vim.keymap.set( "n", "<leader>o",
+					toggle(
+						require('bellows').is_on_closed_fold,
+						require('bellows').fold_closest_block,
+						require('bellows').unfold_closest_block
+					),
+					{ buffer = args.buf, silent = true, }
+				)
+
+				vim.keymap.set( "n", "<leader>i",
+					toggle(
+						require('bellows').is_on_closed_fold,
+						require('bellows').fold_closest_block_recursive,
+						require('bellows').unfold_closest_block_recursive
+					),
+					{ buffer = args.buf, silent = true, }
+				)
+
+				vim.keymap.set( "n", "m",
+					toggle(
+						require('bellows').is_pinned,
+						require('bellows').pin,
+						require('bellows').unpin
+					),
+					{ buffer = args.buf, silent = true, }
+				)
+
+				vim.keymap.set("n", "dm", require('bellows').clear_pins, { buffer = args.buf, silent = true })
+			end,
+		})
+	end,
+}
--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -18,6 +18,17 @@ return {
 		})
 
 		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*.kulala_ui",
+			callback = function(args)
+				local filetype = args.match:gsub("%.kulala_ui$", "")
+				vim.api.nvim_set_option_value("filetype", filetype, { buf = args.buf })
+				vim.api.nvim_set_option_value("buftype", "nofile", { buf = args.buf })
+				vim.api.nvim_set_option_value("bufhidden", "hide", { buf = args.buf })
+				vim.api.nvim_set_option_value("swapfile", false, { buf = args.buf })
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "http",
 			callback = function(args)
 				local buf = args.buf
