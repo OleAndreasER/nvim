d859b405f7a3de08eb6fccf3106450c2c88f49db
OleAndreasERHENT <ole.rorvik@hent.no>
2026-05-01 17:11:00 +0200

    nicer add

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 
-vim.pack.add({ {
+vim.pack.add({
 	src = 'https://github.com/nvim-treesitter/nvim-treesitter',
 	version = 'main',
-} })
+})
 
 require('nvim-treesitter').setup({
 	install_dir = vim.fn.stdpath('data') .. '/site',
