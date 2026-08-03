4459c453aa79324ce90a5bdcd0a9663c57302017
OleAndreasERHENT <ole.rorvik@hent.no>
2026-08-03 09:37:54 +0200

    improve cmp completion window

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,11 +7,48 @@ vim.pack.add({ "https://github.com/hrsh7th/cmp-cmdline" })
 
 vim.pack.add({ "https://github.com/hrsh7th/nvim-cmp" })
 
+vim.pack.add({ "https://github.com/onsails/lspkind.nvim" })
+require('lspkind').init({
+    mode = 'symbol',
+    preset = 'codicons',
+    symbol_map = {
+      Text = "󰉿",
+      Method = "󰆧",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "󰜢",
+      Variable = "󰀫",
+      Class = "󰠱",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "󰑭",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "󰈇",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "󰙅",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "",
+    },
+})
+
+vim.opt.pumheight = 7
+
 require("cmp").setup({
 	snippet = {},
 	window = {},
 	mapping = require("cmp").mapping.preset.insert({
-		["<Tab>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
+		["<Tab>"] = require("cmp").mapping.select_next_item({
+			behavior = require("cmp").SelectBehavior.Select,
+		}),
 		["<Enter>"] = require("cmp").mapping.confirm({
 			behavior = require("cmp").ConfirmBehavior.Insert,
 			select = true,
@@ -25,5 +62,12 @@ require("cmp").setup({
 		{ name = "buffer" },
 		{ name = "path" },
 	}),
+	formatting = {
+		fields = { 'icon', 'abbr', 'menu' },
+		format = require('lspkind').cmp_format({
+			ellipsis_char = '...',
+			show_labelDetails = true,
+		})
+	},
 })
 
